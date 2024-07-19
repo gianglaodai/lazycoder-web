72fc4845f2f65a3c399a7131656e3ad4b1eb37e2
giang hoang <13800000+gianglaodai@users.noreply.github.com>
2024-07-19 16:56:30 +0700

    add blog and demo page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 	import '../app.css';
 </script>
 
+<header class="bg-gray-800 p-4 text-white shadow-md">
+	<div class="container mx-auto flex items-center justify-between">
+		<h1 class="text-2xl font-bold">
+			<a href="/"> Lazycoder </a>
+		</h1>
+		<nav class="space-x-4">
+			<a href="/demo" class="hover:text-gray-400">Live Demo</a>
+			<a href="/blog" class="hover:text-gray-400">Blog</a>
+		</nav>
+	</div>
+</header>
+
 <slot />
---
+++ src/routes/demo/+layout.svelte
@@ -0,0 +1,4 @@
+<div>
+	Demo
+	<slot />
+</div>
